7a72f48f854898b18f4be469362519455c3eeb7b
Changyu Geng <gcyyq@hotmail.com>
2023-03-31 23:25:55 +0800

    optimization

--- projects/ngx-router-outlet-class/src/lib/ngx-router-outlet-class.directive.ts
+++ projects/ngx-router-outlet-class/src/lib/ngx-router-outlet-class.directive.ts
@@ -1,10 +1,10 @@
 import { Directive, Input, OnChanges, OnDestroy, Self, SimpleChanges } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { merge, Subscription } from 'rxjs';
+import { merge, Subscription, tap } from 'rxjs';
 
 @Directive({
-  selector: 'router-outlet[componentClass]',
+  selector: 'router-outlet[componentClass], router-outlet[dynamicClassGetter]',
 })
 export class NgxRouterOutletClassDirective implements OnChanges, OnDestroy {
 
@@ -14,34 +14,104 @@ export class NgxRouterOutletClassDirective implements OnChanges, OnDestroy {
     this.subscription = merge(
       routerOutlet?.activateEvents,
       routerOutlet?.attachEvents,
+    ).pipe(
+      tap(() => this.componentClassList =
+        this.routerOutlet['activated']?.location?.nativeElement.classList),
     ).subscribe({
-      next: () => this.setComponentClass(),
+      next: (component) => {
+        this.setComponentClass();
+
+        const dynamicClasses = this.splitClasses(
+          this.dynamicClassGetter?.(component));
+        if (dynamicClasses) {
+          this.setComponentClass(dynamicClasses);
+        }
+      },
     });
   }
 
-  @Input() componentClass = '';
+  /**
+   * The class(es) to add to the router-outlet's every component's host element.
+   * Multiple classes can be separated by spaces.
+   * It will be applied to the component's host element every time its value changes
+   * or the component changes.
+   * We recommend using this input for static classes for better performance.
+   * @type {string}
+   * @memberof NgxRouterOutletClassDirective
+   */
+  @Input() componentClass?: string;
 
-  subscription: Subscription;
+  /**
+   * A function to get the class(es) for a specific router-outlet's component's host element.
+   * The function is called with the component instance as the only argument every time
+   * the component changes.
+   * The function should return a string with the class(es) to add.
+   * Multiple classes can be separated by spaces.
+   * @type {function}
+   * @memberof NgxRouterOutletClassDirective
+   */
+  @Input() dynamicClassGetter: (component: any) => string = () => '';
 
-  private setComponentClass() {
-    if (this.routerOutlet.isActivated && this.componentClass) {
-      this.routerOutlet['activated'].location?.nativeElement.classList
-        .add(...this.componentClass.split(' '));
-    }
+  private componentClassList: DOMTokenList = this.routerOutlet['activated']?.location?.nativeElement.classList;
+
+  private classArray: string[] = [];
+
+  private subscription: Subscription;
+
+  /**
+   * Split a string of classes into an array of classes.
+   * @param classes A string of classes separated by spaces.
+   * @private
+   */
+  private splitClasses(classes?: string) {
+    return classes?.split(' ').filter(Boolean) || [];
   }
 
-  private unsetComponentClass(classes: string) {
-    if (this.routerOutlet.isActivated && classes) {
-      this.routerOutlet['activated'].location?.nativeElement.classList
-        .remove(...classes.split(' '));
-    }
+  /**
+   * Set the component class(es) to the component's host element.
+   * @param classes
+   * @private
+   */
+  private setComponentClass(classes: string[] = this.classArray) {
+    classes.forEach((className) => {
+      if (!this.componentClassList?.contains(className)) {
+        this.componentClassList?.add(className);
+      }
+    });
+  }
+
+  /**
+   * Unset the component class(es) from the component's host element.
+   * @param classes
+   * @private
+   */
+  private unsetComponentClass(classes: string[]) {
+    classes.forEach((className) => {
+      if (this.componentClassList?.contains(className)) {
+        this.componentClassList?.remove(className);
+      }
+    });
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    if (changes['componentClass']) {
-      this.unsetComponentClass(changes['componentClass'].previousValue);
+    const componentClassChange = changes['componentClass'];
 
-      this.setComponentClass();
+    if (componentClassChange) {
+      const { currentValue, previousValue }: {
+        currentValue?: string,
+        previousValue?: string,
+      } = componentClassChange;
+
+      const previousClasses = this.splitClasses(previousValue);
+      const newClasses = this.splitClasses(currentValue);
+      this.classArray = newClasses;
+
+      const notIn = (target: string[]) => (str: string) => !target.includes(str);
+      const classesToRemove = previousClasses.filter(notIn(newClasses));
+      const classesToAdd = newClasses.filter(notIn(previousClasses));
+
+      this.unsetComponentClass(classesToRemove);
+      this.setComponentClass(classesToAdd);
     }
   }
 
